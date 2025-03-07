5a3e4f7b872a28f300b2e6efaf216e84d08e1838
Sakhi Saswat Panda <113613706+PandaTGOS@users.noreply.github.com>
2025-03-07 09:06:03 +0530

    Update gallery.tsx

--- app/components/gallery.tsx
+++ app/components/gallery.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useRef } from "react"
-import { useInView } from "framer-motion"
 import { SmoothScrollSection, BlurReveal } from "./smooth-scroll"
 import Image from "next/image";
 
